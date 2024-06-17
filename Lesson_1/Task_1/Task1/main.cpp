a6c7ab64a5dc8c5f53dfa5de909743a7ec0824a2
unknown <pachenkomih@gmail.com>
2024-06-17 22:54:30 +0300

    Add .pro

---
+++ Lesson_1/Task_1/Task1/main.cpp
@@ -0,0 +1,10 @@
+#include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Danil" << endl << "Pashchenko" << endl;
+    return 0;
+}
