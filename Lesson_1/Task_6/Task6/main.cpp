919445af1fbb4752f586d0a6370f92f7c43a1ecb
unknown <pachenkomih@gmail.com>
2024-07-06 01:56:47 +0300

    Simplyfied program

--- Lesson_1/Task_6/Task6/main.cpp
+++ Lesson_1/Task_6/Task6/main.cpp
@@ -1,40 +1,33 @@
-#include <QCoreApplication>
 #include <iostream>
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
     int height;
     cout << "Input the height of the tree: ";
     cin >> height;
-    int heightCopy = height - 1;
-    int mainIndex = 0;
 
     cout << endl;
 
-    for(int index = 1; index < height * 2; index += 2)
+    for(int mainIndex = 0; mainIndex < height; mainIndex++)
     {
-        for(int index = 0; index < heightCopy; index++)
+        for(int index = height; index > mainIndex; index--)
         {
             cout << ' ';
         }
-        heightCopy--;
-        while(mainIndex != index)
+        for(int index = 0; index <= (mainIndex * 2); index++)
         {
             cout << '*';
-            mainIndex++;
         }
-        mainIndex = 0;
         cout << endl;
     }
 
-    do
+    for(int index = 0; index < height; index++)
     {
         cout << ' ';
-        mainIndex++;
-    }while(mainIndex != height - 1);
-    cout << '*' << endl;
+    }
+    cout << '*';
 
     return 0;
 }
